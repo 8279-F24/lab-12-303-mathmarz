282d9fbeaec63339a2f267288ed602fe746efc81
Matheus Silva <marz0028@algonquinlive.com>
2024-12-02 17:35:18 -0500

    final

--- Lab12CP.py
+++ Lab12CP.py
@@ -1,3 +1,47 @@
+import time
+import board
+import neopixel
+
+dot_length = input('Enter length of dots between 0 and 1')
+dash_length = (dot_length * 3)
+symbol_gap = dot_length
+character_gap = (dot_length * 3)
+_color = (125, 125, 125)
+brightness = 0.5
+
+
+def light(self, on=True):
+    if on:
+        pixels.fill(self._color)
+    else:
+        pixels.fill((0, 0, 0))
+    pixels.show()
+
+def showDot(self):
+    self.light(True)
+    time.sleep(dot_length)
+    self.light(False)
+    time.sleep(symbol_gap)
+
+def showDash(self):
+    self.light(True)
+    time.sleep(dash_length)
+    self.light(False)
+    time.sleep(symbol_gap)
+
+def display(self, code=".-.-.- "):
+        # iterate through morse code symbols
+        for c in code:
+            # show a dot
+            if c == ".":
+                self.showDot()
+            # show a dash
+            elif c == "-":
+                self.showDash()
+            # show a gap
+            elif c == " ":
+                time.sleep(character_gap)
+
 morse_code_dict = {'A':'.-', 'B':'-...',
     'C':'-.-.', 'D':'-..', 'E':'.',
     'F':'..-.', 'G':'--.', 'H':'....',
@@ -12,13 +56,11 @@ morse_code_dict = {'A':'.-', 'B':'-...',
     '7':'--...', '8':'---..', '9':'----.',
     '0':'-----', 'SPACE' : ' ', 'END-WORD' : '.......'}
 
-# a. prompts the user for a sentence.
 user_input = input("What is the message?")
 
-# b. convert the sentence to lowecase and remove any character/number without a morse code equivalent
 message_upper_case = user_input.upper()
 
-#[MY,NAME,IS]
+
 
 message = message_upper_case.replace("!@#$%^&*()_+-=~`[]{}|;:'\",<.>/?", "").split()
 
@@ -39,5 +81,6 @@ del final[-3:]
 
 s = ''.join(map(str,final))
 
-print(s)
 
+
+display(s)
